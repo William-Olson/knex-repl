617dc25f3134b303486253d40b8f4875c73cb9d3
Will Olson <willko747@gmail.com>
2017-02-19 17:24:19 -0600

    front-end fixes + cleanup

--- public/js/app.js
+++ public/js/app.js
@@ -10,9 +10,20 @@ angular.module('knexrepl', [ ])
     return $http.post('/parse', { code: str });
   };
 
+  service.initEditor = function() {
+    // init ace editor
+    service._editor = ace.edit('editor');
+    service._editor.setTheme('ace/theme/ambiance');
+    service._editor.getSession().setMode('ace/mode/javascript');
+    service._editor.getSession().setTabSize(2);
+    service._editor.getSession().setUseSoftTabs(true);
+    return service._editor;
+  };
+
   return service;
 }])
 
+// filters
 .filter('to_trusted', [ '$sce', function($sce) {
   return function(text) {
       return $sce.trustAsHtml(text);
@@ -20,32 +31,67 @@ angular.module('knexrepl', [ ])
 }])
 
 // controllers
-.controller('mainCtrl', [ '$scope', 'mainSvc', function($scope, mainSvc) {
+.controller('mainCtrl', [ '$scope', 'mainSvc',
 
-  $scope.clear = function() {
-    $scope.editor.setValue('');
-    $scope.output = '';
-    $scope.error = '';
-  };
+  function($scope, mainSvc) {
 
-  $scope.submit = function() {
-    $scope.error = '';
-    mainSvc.getParsed($scope.editor.getValue())
-      .then(r => {
-        $scope.output = r.data;
-      })
-      .catch(err => {
-        console.log('caught err:', err);
-        $scope.error = err.data.message.replace(/\n/gim, '<br>');
-      });
-  };
+    const editor = mainSvc.initEditor();
+    let lastChecked = '';
 
-  // init ace editor
-  $scope.editor = ace.edit('editor');
-  $scope.editor.setTheme('ace/theme/ambiance');
-  $scope.editor.getSession().setMode('ace/mode/javascript');
+    /**
+     * for clearing all displays
+     */
+    $scope.clear = function()
+    {
+      editor.setValue('');
+      $scope.output = '';
+      $scope.error = '';
+      editor.focus();
+    };
 
-}]);
+    /**
+     * submit a query to the backend
+     */
+    const submit = function(input)
+    {
+      mainSvc.getParsed(input)
+        .then(r => {
+          if ($scope.output !== r.data && input === lastChecked) {
+            $scope.error = '';
+            $scope.output = r.data;
+          }
+        })
+        .catch(err => {
+          const toDisplay = err.data.message.replace(/\n/gim, '<br>');
+          if ($scope.error !== toDisplay && input === lastChecked) {
+            $scope.output = '';
+            $scope.error = toDisplay;
+          }
+        });
+    };
+
+    /**
+     * handle editor change event and call submit if needed
+     */
+    editor.getSession().on('change', _.debounce(function() {
+
+      const input = editor.getValue();
+      if (input !== lastChecked) {
+        lastChecked = input;
+        submit(input);
+      }
+
+    }, 800));
+
+    // set some default text & select it
+    editor.setValue('\n\t', 1);
+    editor.insert('knex.from(\'users\')');
+    editor.findPrevious('knex.from(\'users\')');
+    editor.focus();
+
+  }
+
+]);
 
 
 

c3c26e6846977ce09f8c8e8d0297682307aa4ab4
Will Olson <willko747@gmail.com>
2017-02-19 23:45:27 -0600

    don't parse on whitespace changes

--- public/js/app.js
+++ public/js/app.js
@@ -3,21 +3,62 @@
 angular.module('knexrepl', [ ])
 
 // services
-.service('mainSvc', [ '$http', function($http) {
-  const service = {};
+.service('editorSvc', [ '$http', function($http) {
 
-  service.getParsed = function(str) {
+  /**
+   * Note:
+   *  a ref is just a string with all whitespace stripped.
+   *  service._lastRef is used to avoid excessive events
+   *  (ie. when only whitespace or indentation change).
+   */
+  const service = { _lastRef: null };
+  const EDITOR_DEBOUNCE = 420;
+
+  service.parse = function(str) {
     return $http.post('/parse', { code: str });
   };
 
-  service.initEditor = function() {
-    // init ace editor
+  service.getRef = function(str) {
+    return str.replace(/\s|\n/gim, '');
+  };
+
+  service.hasSame = function(str) {
+    const ref = service.getRef(str);
+    return ref === service._lastRef;
+  };
+
+  service.init = function() {
     service._editor = ace.edit('editor');
     service._editor.setTheme('ace/theme/ambiance');
     service._editor.getSession().setMode('ace/mode/javascript');
     service._editor.getSession().setTabSize(2);
     service._editor.getSession().setUseSoftTabs(true);
     return service._editor;
+  };
+
+  service.setDefaultText = function(text) {
+    service._editor.setValue('\n\t', 1);
+    service._editor.insert(text);
+    service._editor.findPrevious(text);
+    service._editor.focus();
+  };
+
+  service.clear = function() {
+    service._editor.setValue('');
+    service._editor.focus();
+  };
+
+  service.onRefChange = function(fn, debounceTime) {
+    service._editor.getSession().on('change', _.debounce(function() {
+        const input = service._editor.getValue();
+        const ref = service.getRef(input);
+
+        if (ref !== service._lastRef) {
+          service._lastRef = ref;
+          fn(input, ref);
+        }
+
+      }, debounceTime || EDITOR_DEBOUNCE));
   };
 
   return service;
@@ -31,22 +72,16 @@ angular.module('knexrepl', [ ])
 }])
 
 // controllers
-.controller('mainCtrl', [ '$scope', 'mainSvc',
-
-  function($scope, mainSvc) {
-
-    const editor = mainSvc.initEditor();
-    let lastChecked = '';
+.controller('mainCtrl', [ '$scope', 'editorSvc', function($scope, editorSvc) {
 
     /**
      * for clearing all displays
      */
     $scope.clear = function()
     {
-      editor.setValue('');
       $scope.output = '';
       $scope.error = '';
-      editor.focus();
+      editorSvc.clear();
     };
 
     /**
@@ -54,16 +89,16 @@ angular.module('knexrepl', [ ])
      */
     const submit = function(input)
     {
-      mainSvc.getParsed(input)
+      editorSvc.parse(input)
         .then(r => {
-          if ($scope.output !== r.data && input === lastChecked) {
+          if ($scope.output !== r.data && editorSvc.hasSame(input)) {
             $scope.error = '';
             $scope.output = r.data;
           }
         })
         .catch(err => {
           const toDisplay = err.data.message.replace(/\n/gim, '<br>');
-          if ($scope.error !== toDisplay && input === lastChecked) {
+          if ($scope.error !== toDisplay && editorSvc.hasSame(input)) {
             $scope.output = '';
             $scope.error = toDisplay;
           }
@@ -71,24 +106,11 @@ angular.module('knexrepl', [ ])
     };
 
     /**
-     * handle editor change event and call submit if needed
+     * handle editor setup and change events
      */
-    editor.getSession().on('change', _.debounce(function() {
-
-      const input = editor.getValue();
-      if (input !== lastChecked) {
-        lastChecked = input;
-        submit(input);
-      }
-
-    }, 800));
-
-    // set some default text & select it
-    editor.setValue('\n\t', 1);
-    editor.insert('knex.from(\'users\')');
-    editor.findPrevious('knex.from(\'users\')');
-    editor.focus();
-
+    editorSvc.init();
+    editorSvc.onRefChange(submit);
+    editorSvc.setDefaultText('knex.from(\'users\')');
   }
 
 ]);
